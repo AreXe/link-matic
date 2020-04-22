21e2727fe9dafb94c08b4d0b73e384c1066a1b54
arexe <arexepro@gmail.com>
2020-04-22 22:13:38 +0200

    Initialize backend project

---
+++ backend/src/test/java/com/arexe/lmatic/LmaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arexe.lmatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
